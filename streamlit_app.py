90ce49f6d17957a05e22b836599bc2c0ff431afb
Adeebbs <aaddy4344@gmail.com>
2025-05-29 15:46:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,5 @@
-import streamlit as st 
+import streamlit as st
 import requests
-
-# Set the app title 
-st.title('My First Streamlit App !!') 
-
-# Add a welcome message 
-st.write('Welcome to my Streamlit app!') 
-
-# Create a text input 
-widgetuser_input = st.text_input('Enter a custom message:', 'Hello, Streamlit!') 
 
 # Set page configuration
 st.set_page_config(page_title="Currency Exchange App", page_icon="💱", layout="centered")
